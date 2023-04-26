018220668e2cf5a0f619c5d65cd0b8425323e980
ulises2125 <ulisesresumenes@gmail.com>
2023-04-25 23:31:59 -0300

    comming css module

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+import styles from './navbar.module.css'
+
+const Navbar = () => {
+    return (
+        <div className={styles.container}>
+            	<h1>Brother</h1>
+                <span>Cursos</span>
+                <span>Quiénes somos</span>
+                <span>Noticias frescas</span>
+                <span>#Wheredoers</span>
+                <span>Contacto</span>
+                <div>
+                <img/>
+                <img/>
+                <img/>
+                </div>
+
+        </div>
+    )
+}
+
+export default Navbar
